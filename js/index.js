74e3f064c985a3294afa726a3f1d10dc931bb496
ga-fleury <gabrielfleury95@gmail.com>
2022-02-07 20:54:57 -0300

    footer
    added footer and background color change on scroll

--- js/index.js
+++ js/index.js
@@ -1,5 +1,8 @@
 import Cursor from "./cursor";
 import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const body = document.querySelector("body");
 
@@ -12,3 +15,22 @@ window.onload = () => {
   });
   const cursor = new Cursor(document.querySelector(".cursor"));
 };
+
+const sectionColors = ["white"]
+const navColors =     ["#FFFFFF","#FCFFF6", "#90EE90", "#EE82EE", "#FF6347"]
+
+gsap.set(".hero", {backgroundColor:gsap.utils.wrap(sectionColors)})
+
+const sections = gsap.utils.toArray(".hero")
+
+sections.forEach(function(section, index){
+	console.log(section, navColors[index])
+	ScrollTrigger.create({
+		trigger:section,
+		start:"top 500px",
+		end:"bottom 10px",
+		animation:gsap.to(".hero", {backgroundColor:navColors[index], immediateRender:false}),
+		toggleActions:"restart none none reverse"
+		
+	})
+})
